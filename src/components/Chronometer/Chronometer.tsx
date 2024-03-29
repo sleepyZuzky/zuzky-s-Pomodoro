989807444d0b0b28120dd9f03017df96c8c1725e
Zuzky <sleepyzuzki@gmail.com>
2024-03-29 17:03:07 -0500

    Style chronometer

--- src/components/Chronometer/Chronometer.tsx
+++ src/components/Chronometer/Chronometer.tsx
@@ -1,8 +1,39 @@
+import styles from './styles.module.css';
+import { useEffect, useState } from 'react';
 
-export default function Chronometer() {
+export default function Chronometer () {
+  const TIMER_RADIUS: number = 170;
+  
+  const [timer, setTimer] = useState(40);
+  const [totalTime, setTotalTime] = useState(50);
+  const [dashOffset, setDashOffset] = useState(0);
+  
+  useEffect((): void => {
+    const circle: SVGCircleElement = document.getElementsByTagName('circle')[0]
+    const ratio = timer / totalTime;
+    console.log(circle.getTotalLength());
+    setDashOffset((1 - ratio) * circle.getTotalLength());
+  }, [timer]);
+  
   return <section id={'chronometer'}>
-    start
-    pause
-    restart
+    <div id="outter_circle" className={styles.outer_circle}>
+      <svg className={styles.loading_container}>
+        <circle cx={'50%'} cy={'50%'} r={TIMER_RADIUS}
+                strokeLinecap="round"
+                className={styles.loading_bar}
+                strokeDasharray={2 * Math.PI * TIMER_RADIUS}
+                strokeDashoffset={dashOffset}/>
+      </svg>
+      <div id="chronometer_container" className={styles.chronometer}>
+        <div>
+          <span>25</span>:<span>50</span>
+        </div>
+        
+        <div id="actions" className={styles.actions}>
+          <span>pause</span>
+        </div>
+      </div>
+    
+    </div>
   </section>
 }
