37fae158c784e3712ba05be943427a827f7cbb2d
Zuzky <sleepyzuzki@gmail.com>
2024-03-29 17:04:07 -0500

    Move components into separated file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import Header from '@components/Header/Header';
-import TimerStatus from '@components/TimerStatus/TimerStatus';
-import Chronometer from '@components/Chronometer/Chronometer';
-import Config from '@components/Config/Config';
+import App from '../pages/_app';
 
-export default function Home() {
-  return (
-    <main className="flex flex-col items-center justify-center min-h-screen">
-      <Header />
-      <TimerStatus />
-      <Chronometer />
-      <Config />
-    </main>
-  );
+export default function Home () {
+  return <App />;
 }
