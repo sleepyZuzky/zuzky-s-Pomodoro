6178102dc3b9c45237bbbb5be811e885bfaaadda
Zuzky <sleepyzuzki@gmail.com>
2024-03-29 17:02:42 -0500

    added font awesome icon

--- src/components/Config/Config.tsx
+++ src/components/Config/Config.tsx
@@ -1,6 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGear } from '@fortawesome/free-solid-svg-icons'
 
 export default function Config() {
   return <section id={'configuration'}>
+    <FontAwesomeIcon icon={faGear} />
     Settings
     
     Time (minutes)
