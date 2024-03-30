db35055a944de7507a4d384fa6682f579a4ce2a0
Zuzky <sleepyzuzki@gmail.com>
2024-03-30 07:31:03 -0500

    WIP: Modal styling

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,53 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import styles from './styles.module.css'
+
+export default function Modal({children, header}) {
+  return <dialog data-settings-modal className={styles.modal}>
+    <section id='modal__header' className={[styles.modal__header, styles.separator].join(' ')}>
+      <span>
+        {header}
+      </span>
+      
+      <FontAwesomeIcon icon={faXmark} color={'#ccc'} className={'pointer'} onClick={() => {
+        const modal: HTMLDialogElement = document.querySelector('[data-settings-modal]');
+        modal.close();
+      }}/>
+    </section>
+    
+    <section id='modal__body' className={styles.modal__body}>
+      <div>
+        <span>Time (minutes)</span>
+        
+        <div className={[styles.timers, styles.separator].join(' ')}>
+          <label className={styles.timer}>
+            <span>pomodoro</span>
+            <input type='number' defaultValue={0}/>
+          </label>
+          
+          <label className={styles.timer}>
+            <span>short break</span>
+            <input type='number' defaultValue={0}/>
+          </label>
+          
+          <label className={styles.timer}>
+            <span>long break</span>
+            <input type='number' defaultValue={0}/>
+          </label>
+        </div>
+      </div>
+      
+      <div className={styles.separator}>
+        Font
+      </div>
+      
+      <div>
+        Color
+      </div>
+      
+      <button>
+        Apply
+      </button>
+    </section>
+  </dialog>
+}
