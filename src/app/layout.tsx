b2bb63228e379b7a7968e172c56840bc79699ee5
Zuzky <sleepyzuzki@gmail.com>
2024-03-30 07:30:25 -0500

    Added Vercel SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { NextFont } from 'next/dist/compiled/@next/font';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter: NextFont = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <SpeedInsights />
     </html>
   );
 }
