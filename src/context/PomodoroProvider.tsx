88adb52196f431e72fd87a99b36d7d1d5a16c654
Zuzky <sleepyzuzki@gmail.com>
2024-04-01 05:01:21 -0500

    Added Provider to handle state

---
+++ src/context/PomodoroProvider.tsx
@@ -0,0 +1,52 @@
+import {
+  Context,
+  createContext,
+  Dispatch, ReactNode,
+  SetStateAction, useEffect,
+  useState
+} from 'react';
+
+export type PomodoroFont = 'sans-serif' | 'sans' | 'monospace';
+export type PomodoroColor = '#F87070' | '#70F3F8' | '#D881F8';
+
+interface PomodoroContextType {
+  font: PomodoroFont,
+  color: PomodoroColor
+  pomodoro: number,
+  shortBreak: number,
+  longBreak: number,
+}
+
+const defaultContext: PomodoroContextType = {
+  font: 'sans',
+  color: '#F87070',
+  pomodoro: 25,
+  shortBreak: 5,
+  longBreak: 15
+}
+
+export const PomodoroContext: Context<{
+  pomodoro: PomodoroContextType,
+  setPomodoro: (pomodoro: PomodoroContextType) => void
+}> = createContext({
+  pomodoro: defaultContext,
+  setPomodoro: (pomodoro: PomodoroContextType) => {},
+});
+
+export default function PomodoroProvider ({children}: {children: ReactNode}) {
+  const [pomodoro, setPomodoro] = useState(defaultContext);
+  
+  useEffect(() => {
+    localStorage.setItem('config', JSON.stringify(pomodoro));
+  }, [pomodoro]);
+  
+  useEffect((): void => {
+    const config: string | null = localStorage.getItem('config');
+    const storageData = config ? Object.keys(JSON.parse(config)).length === 0 ? defaultContext : JSON.parse(config) : defaultContext;
+    setPomodoro(storageData);
+    }, []);
+  
+  return <PomodoroContext.Provider value={{pomodoro, setPomodoro}}>
+    {children}
+  </PomodoroContext.Provider>;
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
@@ -6,16 +6,21 @@ import Header from '@components/Header/Header';
 import TimerStatus from '@components/TimerStatus/TimerStatus';
 import Chronometer from '@components/Chronometer/Chronometer';
 import Config from '@components/Config/Config';
+import PomodoroProvider from '../context/PomodoroProvider';
+import Modal from '@components/Modal/Modal';
 
 config.autoAddCss = false;
 
 export default function App () {
   return <main className="flex flex-col items-center justify-evenly w-screen min-w-screen h-screen min-h-screen">
-    <section id="pomodoro_container" className='flex flex-col items-center justify-evenly w-50 h-screen'>
+    <section id="pomodoro_container" className="flex flex-col items-center justify-evenly w-50 h-screen">
       <Header/>
-      <TimerStatus/>
-      <Chronometer/>
-      <Config/>
+      <PomodoroProvider>
+        <TimerStatus/>
+        <Chronometer/>
+        <Config/>
+        <Modal header="Settings"/>
+      </PomodoroProvider>
     </section>
-  </main>;
+  </main>
 };
