be23b261ab25c5d6b3af7c129af3a82b5a236e9d
Zuzky <sleepyzuzki@gmail.com>
2024-04-01 05:01:42 -0500

    Added temporal styles to header buttons

--- src/components/TimerStatus/TimerStatus.tsx
+++ src/components/TimerStatus/TimerStatus.tsx
@@ -1,8 +1,15 @@
-
-export default function TimerStatus() {
-  return <section id={'timer_status'} className={'flex'}>
-    pomodoro
-    short break
-    long break
+export default function TimerStatus () {
+  return <section id={'timer_status'} className={'flex bg-red-500 px-5 py-5 rounded-full'}>
+    <span className={'bg-yellow-600 pt-4 pb-3.5 px-6 rounded-full'}>
+      pomodoro
+    </span>
+    
+    <span className={'bg-yellow-600 pt-4 pb-3.5 px-6 rounded-full'}>
+      short break
+    </span>
+    
+    <span className={'bg-yellow-600 pt-4 pb-3.5 px-6 rounded-full'}>
+      long break
+    </span>
   </section>
 }
