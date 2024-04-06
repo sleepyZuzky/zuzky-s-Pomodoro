4d55e11b54c92f908c75a5000e6791bd9a15d23a
Zuzky <sleepyzuzki@gmail.com>
2024-04-06 15:39:04 -0500

    fix (app): Remove unused prop

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,7 +5,7 @@ import ModalHeader from '@components/Modal/ModalHeader/ModalHeader';
 import ModalBody from '@components/Modal/ModalBody/ModalBody';
 import ModalFooter from '@components/Modal/ModalFooter/ModalFooter';
 
-export default function Modal ({header}: { header: string }) {
+export default function Modal () {
   return <dialog data-settings-modal className={styles.modal}>
     <ModalHeader label={'Settings'} />
     
